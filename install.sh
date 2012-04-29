2d76ecd2f13fb9eda86fa2ff986d0ccf4eb7e8dd
Lucas Caton <lucascaton@gmail.com>
2012-04-29 15:42:11 -0300

    Moving files to 'files' directory

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ files=(bash_profile gitconfig irbrc psqlrc rvmrc)
 echo -ne '=> Backup your dotfiles'
 for ((i = 0; i < ${#files[@]}; i++)) do
   echo -ne '.'; sleep 0.1
-  if [[ -s ~/.${files[$i]} ]]; then mv ~/.${files[$i]} ~/.${files[$i]}.bkp; fi
+  if [ -s ~/.${files[$i]} ]; then mv ~/.${files[$i]} ~/.${files[$i]}.bkp; fi
 done; echo -ne '\n'
 
 echo -ne '=> Create symlinks to new dotfiles'
 for ((i = 0; i < ${#files[@]}; i++)) do
   echo -ne '.'; sleep 0.1
-  ln -s ~/.dotfiles/${files[$i]} ~/.${files[$i]}
+  ln -s ~/.dotfiles/files/${files[$i]} ~/.${files[$i]}
 done; echo -ne '\n'
 
 echo -ne '\nDone :-)\n\n'
