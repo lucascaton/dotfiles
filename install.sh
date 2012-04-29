e7a26a0d0e22d7be0d1c72d99f580980c8f987d7
Lucas Catón <lucascaton@gmail.com>
2012-04-29 14:03:30 -0300

    Include irbrc

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ echo 'Installing...'
 echo 'Backup your dotfiles...'
 if [[ -s ~/.bash_profile ]] ; then mv ~/.bash_profile ~/.bash_profile.bkp ; fi
 if [[ -s ~/.gitconfig ]] ; then mv ~/.bash_profile ~/.gitconfig.bkp ; fi
+if [[ -s ~/.irbrc ]] ; then mv ~/.bash_profile ~/.irbrc.bkp ; fi
 
 echo 'Create symlinks to new dotfiles...'
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/irbrc ~/.irbrc
