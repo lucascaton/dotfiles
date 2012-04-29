dbbcded4105636442234c27a64bec1d59c221078
Lucas Catón <lucascaton@gmail.com>
2012-04-29 13:54:50 -0300

    Include gitconfig dotfile

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 echo 'Installing...'
 
 echo 'Backup your dotfiles...'
-# if [[ -s ~/.bash_profile ]] ; then mv ~/.bash_profile ~/.bash_profile.bkp ; fi
+if [[ -s ~/.bash_profile ]] ; then mv ~/.bash_profile ~/.bash_profile.bkp ; fi
+if [[ -s ~/.gitconfig ]] ; then mv ~/.bash_profile ~/.gitconfig.bkp ; fi
 
 echo 'Create symlinks to new dotfiles...'
-# ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
