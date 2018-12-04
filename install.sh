4ba0b2763fac264a426f0adb0d5db75531cde331
Lucas Caton <lucascaton@gmail.com>
2018-12-04 18:55:39 +1100

    Improve 'install.sh' script

--- install.sh
+++ install.sh
@@ -1,28 +1,53 @@
 #! /bin/bash
 
-NO_COLOR="\033[1;0m"
+ORIGINAL_COLOR="\033[1;0m"
 GREEN="\033[0;32m"
-GRAY="\033[1;30m"
+GRAY="\033[2m"
+RED="\033[31m"
 
-echo -ne "\n${GREEN}Initializing...\n\n"
-echo -e "Creating backup & creating symlinks to new dotfiles..."
+title() {
+  echo -e "\n${GREEN}${1}...${ORIGINAL_COLOR}\n"
+}
+
+# ------------------------------------------------------------------------------
+
+title "Creating backup & creating symlinks to new dotfiles"
 
 cd ~/.dotfiles/files
 for file in *; do
-  echo "~/.$file"
-  if [ -s ~/.$file ]; then mv ~/.$file ~/.$file.bkp; fi
-  ln -s ~/.dotfiles/files/$file ~/.$file
+  echo -ne "~/.$file ${GRAY}"
+
+  if [ -s ~/.$file ]; then
+    if ! diff -q ~/.$file ~/.dotfiles/files/$file &>/dev/null; then
+      mv ~/.$file ~/.$file.bkp
+      ln -s ~/.dotfiles/files/$file ~/.$file
+      echo -ne "has been moved to ~/.$file.bkp and a new symlink has been created ${GREEN}✓${ORIGINAL_COLOR}\n"
+    else
+      echo -ne "is identical ${RED}✕${ORIGINAL_COLOR}"
+    fi
+  else # File doesn't exist, let's create it:
+    echo -ne "symlink has been created ${GREEN}✓${ORIGINAL_COLOR}"
+    ln -s ~/.dotfiles/files/$file ~/.$file
+  fi
+
+  echo -e "${ORIGINAL_COLOR}"
 done
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  ~/.dotfiles/other_files/macOS
-fi
+# ------------------------------------------------------------------------------
+
+title "Setting up macOS configs"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then ~/.dotfiles/other_files/macOS; fi
+
+# ------------------------------------------------------------------------------
+
+title "Installing Atom packages"
 
 if [ -d ~/.atom ]; then
   apm install --packages-file ~/.dotfiles/other_files/atom/default/package.list;
   apm install --packages-file ~/.dotfiles/other_files/atom/course/package.list;
 fi
 
-echo -e "${GRAY}"
+# ------------------------------------------------------------------------------
 
-echo -ne "${GREEN}Done! :-)${NO_COLOR}\n\n"
+echo -e "\n${GREEN}Done!${ORIGINAL_COLOR}\n"
