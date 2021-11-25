20d8649af6bc9fd175b1fe3be2cc5677a31dc229
Lucas Caton <lucascaton@gmail.com>
2021-11-25 17:26:09 +1000

    Replace 'powerlevel9k' with 'powerlevel10k' in the install script

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ title "Installing ZSH plugins"
 
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions       ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/bhilburn/powerlevel9k.git           ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel9k
+git clone https://github.com/romkatv/powerlevel10k.git           ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # ------------------------------------------------------------------------------
 
