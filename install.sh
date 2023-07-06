1afe76dfb3e84907bea9aecd8b65e10b2837a9c4
Lucas Caton <lucascaton@gmail.com>
2023-07-06 18:10:31 +1000

    Remove duplicated code

--- install.sh
+++ install.sh
@@ -57,12 +57,4 @@ if [[ "$OSTYPE" == "darwin"* ]]; then brew bundle --file=~/.dotfiles/other_files
 
 # ------------------------------------------------------------------------------
 
-# title "Installing ZSH plugins"
-
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# git clone https://github.com/zsh-users/zsh-autosuggestions         ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# git clone https://github.com/romkatv/powerlevel10k.git             ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# ------------------------------------------------------------------------------
-
 echo -e "\n${GREEN}Done!${ORIGINAL_COLOR}\n"
